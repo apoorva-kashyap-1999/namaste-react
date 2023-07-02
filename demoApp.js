e71f6b017473bd4ed0398b7c999913699723db42
apoorva <apkpashu@gmail.com>
2023-07-02 22:46:49 +0530

    deleting redundant file

--- demoApp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from"react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "firstChild" }, [React.createElement("h1", { id: "secondChild}" }, "Namaste React🚀 "), React.createElement("h2", { id: "secondChild" }, "I am an H2 tag")],[React.createElement("h1", { id: "secondChild}" }, "I am an H1 tag"), React.createElement("h2", { id: "secondChild" }, "I am an H2 tag")]))
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
